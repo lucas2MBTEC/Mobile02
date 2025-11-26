9506bc87b5316dd1464dd54aa45429f1fc19f2a5
Lucas araújo <lucas.zimovski.brasil@escola.pr.gov.br>
2025-11-26 09:54:46 -0300

    wf

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,71 @@
-import { useState } from "react";
+function App(){
+const[task,setTask] = useState(
+    [
+        {
+            id: 1,
+            title:"Estudar programação"
+            description: "Estudar programação para se tornar um desenvolvefor full stack ",
+isCompleted:false
+
+        }
+        {
+            id: 2,
+            title:"Estudar em inglês ",
+            description:"para se tornar fluente",
+            isCompleted: false
+        }
+        {
+id: 3,
+title:"Estudar matemática",
+description:"Estudar matemática para melhorar minha lógica",
+isCompleted: false
+
+        }
+
+    ]);
+
+return (
+    <h1>Gerenciando tarefas</h1>
+);
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { useState } from "react";
 function App() {
 const[message, setMessage] = useState('olá mundo');
 return(
